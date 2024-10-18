b9258b1f401833a09d4607847637012eee9aa4b9
Katharine Hyatt <hyatkath@amazon.com>
2024-10-18 12:15:48 -0400

    add in the rest of the device names, arns, and qubit counts

--- runtime/cudaq/platform/default/rest/helpers/braket/BraketServerHelper.cpp
+++ runtime/cudaq/platform/default/rest/helpers/braket/BraketServerHelper.cpp
@@ -20,12 +20,21 @@ namespace cudaq {
 const std::string SV1 = "sv1";
 const std::string DM1 = "dm1";
 const std::string TN1 = "tn1";
+const std::string Aria1 = "aria1";
+const std::string Aria2 = "aria2";
+const std::string Garnet = "garnet";
+const std::string Aquila = "aquila";
 
 const std::string sv1_arn = "arn:aws:braket:::device/quantum-simulator/amazon/sv1";
 const std::string dm1_arn = "arn:aws:braket:::device/quantum-simulator/amazon/dm1";
 const std::string tn1_arn = "arn:aws:braket:::device/quantum-simulator/amazon/tn1";
+const std::string aria1_arn = "arn:aws:braket:us-east-1::device/qpu/ionq/Aria-1";
+const std::string aria2_arn = "arn:aws:braket:us-east-1::device/qpu/ionq/Aria-2";
+const std::string garnet_arn = "arn:aws:braket:eu-north-1::device/qpu/iqm/Garnet";
+const std::string aquila_arn = "arn:aws:braket:us-east-1::device/qpu/quera/Aquila";
 
-const std::map<std::string, uint> Machines = {{SV1, 34}, {DM1, 17}, {TN1, 50}};
+// Do we need to add the `device_name.txt` files for these?
+const std::map<std::string, uint> Machines = {{SV1, 34}, {DM1, 17}, {TN1, 50}, {Aria1, 25}, {Aria2, 25}, {Garnet, 20}, {Aquila, 256}};
 
 /// @brief The BraketServerHelper class extends the ServerHelper class to handle
 /// interactions with the Amazon Braket server for submitting and retrieving quantum
